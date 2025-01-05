31ac6b959190d9cd3d445e3a999306fca2ea9277
Aryan Raj <aryanraj1622@gmail.com>
2025-01-05 20:08:05 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,15 +2,19 @@ const express=require("express");
 
 const app=express();
 
-app.use("/test", (req, res)=>{
-    res.send("Hello from server");
+app.get("/user", (req, res)=>{
+    res.send({"firstName": "Akshay", "lastName": " Saini"});
 });
 
-app.use("/", (req, res)=>{
-    res.send("Hello from dashboard");
-})
+app.post("/user", (req, res)=>{
+    //saving data to db
+    console.log("Saved data to database");
+    res.send("Data successfully saved");
+});
 
-
+app.use("/test", (req, res)=>{
+    res.send("Hello from test");
+});
 
 app.listen(3000, ()=>{
     console.log("Server listening on port 3000");
