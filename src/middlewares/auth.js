9a934c1d0924b6e009e8463e0b1a7f0e905cbdb9
Aryan Raj <aryanraj1622@gmail.com>
2025-01-17 19:46:07 +0530

    jwt

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,14 +1,26 @@
-const adminAuth= (req, res, next)=>{
-    console.log("Hi I am in admin auth");
-    const token="xyz";
-    const isAuthenticated= token==="xyz";
-    if(!isAuthenticated){
-        res.status(401).send("Not Authorized");
-    }else{
+const jwt= require("jsonwebtoken");
+const {User}= require("../models/user");
+
+const userAuth= async(req, res, next)=>{
+    try{
+        const cookies= req.cookies;
+        const{token}=cookies;
+        if(!token){
+            throw new Error("Token is not valid");
+        }
+        const decodedObj= await jwt.verify(token, "DEV@Tinder#123");
+        const{_id}=decodedObj;
+        const user= await User.findById(_id);
+        if(!user){
+            throw new Error("User not found");
+        }
+        req.user=user;
         next();
+    }catch(err){
+        res.status(400).send("ERROR: "+err.message);
     }
 }
 
 module.exports={
-    adminAuth,
+    userAuth,
 }
